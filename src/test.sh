f18ecaa4d51d191b305ab40d3a382206b2f6a5c7
sondos mohammed <sondos.mohammed0100@gmail.com>
2026-06-03 11:55:20 +0300

    adding test

---
+++ src/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# src/test.sh
+EXPECTED="Hello, Test!"
+
+
+OUTPUT=$(node -e "
+  const greet = require('./src/app');
+  console.log(greet('Test'));
+")
+
+if [ "$OUTPUT" = "$EXPECTED" ]; then
+  echo "✅ Test passed!"
+  exit 0
+else
+  echo "❌ Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+  exit 1
+fi
